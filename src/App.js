11ab68d5f2ee402763f9749e9ac577efb6aebdef
ShibinaNoushad <shibinanoushad2224@gmail.com>
2022-12-21 12:26:15 +0530

    Meals Summary Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./Components/Layout/Header";
+import MealsSummary from "./Components/Meals/MealsSummary";
 function App() {
   return (
     <div>
       <Header></Header>
+      <MealsSummary></MealsSummary>
     </div>
   );
 }
